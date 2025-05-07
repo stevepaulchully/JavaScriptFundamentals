ebc2ea793adcbb9421955e8d0dedcfe2483690a0
stevepaulchully <steve.p@neoito.com>
2025-05-07 14:34:22 +0530

    CustomErrors

---
+++ 10.1customErrors.js
@@ -0,0 +1,14 @@
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+let err = new FormatError("formatting error");
+
+alert( err.message ); // formatting error
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
+
+alert( err instanceof SyntaxError ); // true
